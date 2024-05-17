0501b4daeb63c825ea1048631a34108ed1c46eee
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-05-16 21:28:57 -0300

    chore: add eslint prettier and build configs

---
+++ .eslintrc.js
@@ -0,0 +1,101 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [
+    '.eslintrc.js',
+    'commitlint.config.js',
+    '.prettierrc.js',
+    'dist/**/*',
+  ],
+  rules: {
+    'sort-imports': [
+      'error',
+      { ignoreCase: true, ignoreDeclarationSort: true },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@nestjs/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@app/configs/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@app/database/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@app/models/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@app/functions/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@app/shared/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@app/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'max-len': ['error', { code: 80, ignorePattern: '^import .*' }],
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        'no-spaced-func': 'off',
+      },
+    },
+  ],
+};
