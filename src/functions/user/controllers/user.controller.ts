57d8d5219f0dad28076d38e9df505bdec39f3279
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-06-04 20:18:15 -0300

    feat: add method on controller to find user by id

--- src/functions/user/controllers/user.controller.ts
+++ src/functions/user/controllers/user.controller.ts
@@ -1,10 +1,24 @@
-import { Get, JsonController } from 'routing-controllers';
+import { Get, JsonController, Param } from 'routing-controllers';
 import { OpenAPI } from 'routing-controllers-openapi';
+import { Inject } from 'typedi';
+
+import {
+  ApiResponse,
+  GetResponseApi,
+  GetResponseApiFalse,
+} from '@app/shared/middleware';
+
+import { UserService } from '../services/user.service';
+
+import { UserControllerDTO } from './dto/user.dto';
 
 @JsonController()
 @OpenAPI({ tags: ['UserController'] })
 export class UserControllerAuthorized {
-  constructor() {}
+  constructor(
+    @Inject()
+    private readonly userService: UserService,
+  ) {}
 
   @OpenAPI({
     visible: true,
@@ -13,5 +27,16 @@ export class UserControllerAuthorized {
   @Get('/v1/user')
   public async getListUsers() {
     return ['user1', 'user2'];
+  }
+  @Get('/v1/user/:userId')
+  public async getUserById(
+    @Param('userId') userId: string,
+  ): Promise<ApiResponse<UserControllerDTO.Output.UserById>> {
+    try {
+      const user = await this.userService.getUserById(userId);
+      return GetResponseApi(user);
+    } catch (error: any) {
+      return GetResponseApiFalse([error.message]);
+    }
   }
 }
