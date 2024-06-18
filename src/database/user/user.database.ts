aa6fef1d7d91c8fee72b130af09cf15fd3a9018a
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-06-18 19:52:47 -0300

    refactor: add properties of schema in user.database

--- src/database/user/user.database.ts
+++ src/database/user/user.database.ts
@@ -1,6 +1,13 @@
 import { Schema } from 'mongoose';
 
-import { UserEntity } from '@app/core';
+import {
+  USER_EMAIL_MAX_LENGTH,
+  USER_NAME_MAX_LENGTH,
+  USER_NAME_MIN_LENGTH,
+  USER_PASSWORD_LENGTH,
+  UserEntity,
+} from '@app/core';
+import { getMongoConnection } from '@app/shared';
 
 import { IDatabase } from '../base';
 
@@ -10,7 +17,44 @@ export class UserDatabase implements IDatabase<DTO> {
   public keyId = 'userId';
   public collectionName = 'users';
 
-  public schema = new Schema<DTO>({});
+  public getModel = async () => {
+    const mongoConnection = await getMongoConnection();
+    const model = mongoConnection.model<DTO>(this.collectionName, this.schema);
+
+    return model;
+  };
+
+  public schema = new Schema<DTO>({
+    userId: {
+      type: String,
+      immutable: true,
+    },
+
+    email: {
+      type: String,
+      required: [true, 'NOT_EMPTY'],
+      max: USER_EMAIL_MAX_LENGTH,
+    },
+
+    userName: {
+      type: String,
+      required: [true, 'NOT_EMPTY'],
+      max: USER_NAME_MAX_LENGTH,
+      min: [USER_NAME_MIN_LENGTH, 'NAME_MIN_LENGTH_3'],
+    },
+
+    password: {
+      type: String,
+      required: false,
+      min: USER_PASSWORD_LENGTH,
+      max: USER_PASSWORD_LENGTH,
+    },
+
+    image: {
+      type: String,
+      required: false,
+    },
+  });
 
   public index = async () => {
     this.schema
