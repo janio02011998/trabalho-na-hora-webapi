f15aeea6726406c0e1b3bc437b7ea59b00873f1a
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-06-19 20:07:57 -0300

    feat: add config and import env vars to mongo database model

--- src/shared/model/base.model.ts
+++ src/shared/model/base.model.ts
@@ -1,15 +1,39 @@
 import mongoose, { Connection, ConnectOptions } from 'mongoose';
+import 'dotenv/config';
 
 import { LoggerServiceHelper } from '../service';
 
 let mongoConnection: Connection;
 
 const getConnectionString = (): string => {
-  return '';
+  return process.env.MONGO_CONNECTION_STRING;
 };
 
 const getConnectionConfig = (): ConnectOptions => {
-  return {} as ConnectOptions;
+  const dbName = process.env.MONGO_DATABASE_NAME;
+  const appName = process.env.APP_NAME;
+
+  const baseConnectionConfig = {
+    ssl: false,
+    tls: false,
+
+    dbName,
+    appName,
+
+    minPoolSize: 1,
+    maxPoolSize: 1,
+
+    retryReads: true,
+    readConcernLevel: 'majority',
+
+    writeConcern: 'majority',
+    retryWrites: false,
+
+    user: process.env.MONGO_USER,
+    pass: process.env.MONGO_PASSWORD,
+  } as ConnectOptions;
+
+  return baseConnectionConfig;
 };
 
 export const getMongoConnection = async (): Promise<Connection> => {
