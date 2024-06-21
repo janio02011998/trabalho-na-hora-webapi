88744441e78106a1fc0dbc1d4ef43232fb57d65b
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-06-21 20:26:00 -0300

    refactor: extends method of UserRepository

--- src/functions/user/data/user.repository.ts
+++ src/functions/user/data/user.repository.ts
@@ -1,26 +1,27 @@
-import { Inject, Service } from 'typedi';
+import { Service } from 'typedi';
 
-import { LoggerServiceHelper } from '@app/shared/service';
+import { UserEntity } from '@app/core';
+import { UserRepository } from '@app/database';
 
 @Service({ global: true })
-export class UserRepo {
-  constructor(
-    @Inject()
-    private readonly loggerService: LoggerServiceHelper,
-  ) {}
+export class UserRepo extends UserRepository {
+  constructor() {
+    super();
+  }
 
-  public async getUserById(id: string): Promise<any> {
+  public async getUserById(id: string): Promise<UserEntity.Type | undefined> {
     try {
       return new Promise(resolve => {
         resolve({
           userId: id,
-          name: 'name',
+          userName: 'name',
           email: 'email',
           image: 'image',
         });
       });
     } catch (error) {
       this.loggerService.error(error, 'UserRepo::getUserById', [id]);
+      return undefined;
     }
   }
 }
